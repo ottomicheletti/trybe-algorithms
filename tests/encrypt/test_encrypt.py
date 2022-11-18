287372816328ae213015deec08106fa81f398ea9
ottomicheletti <michelettiotto@gmail.com>
2022-11-18 17:49:48 -0300

    test(Req 02): :test_tube: Adds a test to verify encrypt_message function

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
-def test_encrypt_message():
-    pass
+@pytest.mark.parametrize(
+    "message, key, expected",
+    [
+        ("AABBCC", 3, "BAA_CCB"),
+        ("ABBCCA", 4, "AC_CBBA"),
+        ("AABBCC", -1, "CCBBAA"),
+    ],
+)
+def test_encrypt_message(message, key, expected):
+    assert encrypt_message(message, key) == expected
