63d11cafe9183c7a1139ba6aa17a89dcdeb362b8
ottomicheletti <michelettiotto@gmail.com>
2022-11-18 17:36:49 -0300

    feat(Req 01): :sparkles: Defines study_schedule within challenge_study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period, target_time=None):
+    students = 0
+
+    if not (isinstance(target_time, int)):
+        return None
+
+    for arrival, exit in permanence_period:
+        if not (isinstance(arrival, int) and isinstance(exit, int)):
+            return None
+        if arrival <= target_time <= exit:
+            students += 1
+
+    return students
