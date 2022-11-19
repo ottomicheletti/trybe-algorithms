5ec4fe6d5989c02342b878c03aa2af1e7f2c0641
ottomicheletti <michelettiotto@gmail.com>
2022-11-19 14:51:35 -0300

    perf(Req 04): :zap: Changes the bubble_sort method to the quick_sort method.
    With this change the previous mentioned 20 tests continue to pass, but now, with lower complexity, despite being more verbose, the final complexity test passes too.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,18 +1,28 @@
-def bubble_sort(arr):
-    array = arr
-    n = len(arr)
-    for i in range(n):
-        for j in range(n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+# Inspired by https://www.geeksforgeeks.org/python-program-for-quicksort/
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+
+
+def quick_sort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quick_sort(array, low, pi - 1)
+        quick_sort(array, pi + 1, high)
     return array
 
 
 def is_anagram(first_string, second_string):
     first_array = list(first_string.lower())
     second_array = list(second_string.lower())
-    first_array_sorted = bubble_sort(first_array)
-    second_array_sorted = bubble_sort(second_array)
+    first_array_sorted = quick_sort(first_array, 0, len(first_array) - 1)
+    second_array_sorted = quick_sort(second_array, 0, len(second_array) - 1)
 
     if first_string == "" or second_string == "":
         return (
