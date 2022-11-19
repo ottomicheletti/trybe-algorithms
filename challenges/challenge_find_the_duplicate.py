97ff1b410f7512a4cf847d2c45a1bb8a4ceb99e2
ottomicheletti <michelettiotto@gmail.com>
2022-11-19 16:55:58 -0300

    feat(Req 05): :sparkles: Implements find_duplicate function within challenge_find_the_duplicate.py
    This solution can easily pass 6 os the 7 tests. The last one I couldn't run locally because my computer won't cooperate with execution time.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,22 @@
+from statistics import mode
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or not isinstance(nums, list):
+        return False
+
+    _nums = []
+
+    for n in nums:
+        if n not in _nums:
+            _nums.append(n)
+
+    result = mode(nums)
+
+    if not isinstance(result, int) or result < 0:
+        return False
+
+    if len(nums) != len(_nums):
+        return mode(nums)
+    else:
+        return False
