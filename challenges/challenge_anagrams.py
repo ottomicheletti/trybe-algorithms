30819a757f94b254f5825eced7591dfe20cf2740
ottomicheletti <michelettiotto@gmail.com>
2022-11-19 14:46:34 -0300

    feat(Req 04): :sparkles: Defines is_anagram function within challenge_anagrams.py
    In this implementation, the chosen method for sorting the string is the bubble sort. It runs well and passes 20 tests, but fails for complexity.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,28 @@
+def bubble_sort(arr):
+    array = arr
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return array
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_array = list(first_string.lower())
+    second_array = list(second_string.lower())
+    first_array_sorted = bubble_sort(first_array)
+    second_array_sorted = bubble_sort(second_array)
+
+    if first_string == "" or second_string == "":
+        return (
+            "".join(first_array_sorted),
+            "".join(second_array_sorted),
+            False,
+        )
+
+    return (
+        "".join(first_array_sorted),
+        "".join(second_array_sorted),
+        first_array_sorted == second_array_sorted,
+    )
